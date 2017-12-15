6cfcd650d2fb9a19a67a42661b68376477147619
Sathvik777 <katam.sathvik@gmail.com>
2017-12-15 15:13:24 +0100

    new game logic implemented

--- hangman/static/js/index.js
+++ hangman/static/js/index.js
@@ -1,42 +1,50 @@
-var socket = io('http://' + document.domain + ':' + location.port);
+//var socket = io('http://' + document.domain + ':' + location.port);
 var angularApp = angular.module('hangmanApp', []);
 var serverUrl = 'http://' + document.domain + ':' + location.port;
 var sessionKey = localStorage.getItem('sessionKey');
 var username = localStorage.getItem('username');
-  
 
-  angularApp.controller('loginController', ['$scope', function($scope) {
-    console.log("loging ");
-    //socket = io.connect('http://' + document.domain + ':' + location.port);
-    if(sessionKey !== null)$scope.name = username;
-    $scope.myFunc = function() {
-      $.get( serverUrl+"/login?username="+$scope.name, function( autunticationResponse ) {
-        $( "#loginView" ).hide();
-        $scope.$broadcast('logedin', );
-        console.log(autunticationResponse);
-        localStorage.setItem("sessionKey", autunticationResponse.sessionKey);
-        localStorage.setItem("username", autunticationResponse.username);
-      });
-    };
 
-    /*socket.on('word', function(data){
-            console.log(data);
-        });
-     */   
-  }]);
+angularApp.controller('loginController', ['$scope', function ($scope) {
+  console.log("loging ");
+  //socket = io.connect('http://' + document.domain + ':' + location.port);
+  if (sessionKey !== null) $scope.name = username;
+  $scope.myFunc = function () {
+    $.get(serverUrl + "/login?username=" + $scope.name, function (autunticationResponse) {
+      $("#loginView").hide();
+      $scope.$broadcast('logedin', );
+      console.log(autunticationResponse);
+      localStorage.setItem("sessionKey", autunticationResponse.sessionKey);
+      localStorage.setItem("username", autunticationResponse.username);
+    });
+  };
 
-  angularApp.controller('displayWord', ['$scope', function($scope) {
-      $scope.on('logedin', function(event, args){
-        console.log("logedin");
-        socket = io.connect();
-        socket.emit('start',{
-          "session_key" : sessionKey
-        } );
-
-        socket.on('started',function(data){
+  /*socket.on('word', function(data){
           console.log(data);
-        } );
-
       });
+   */
+}]);
 
-  }]);
+angularApp.controller("gameController", ['$scope', function ($scope) {
+  // TODO: Get word length or word from server 
+  var word = "3dhubs";
+  $scope.inCorrectLetters = [];
+  $scope.correctLeters = [];
+  $scope.guessLimit = 6;
+  $scope.displayWord = '';
+  $scope.input = {
+    letter: ''
+  }
+
+  var newGame = function () {
+    $scope.inCorrectLetters = [];
+    $scope.correctLeters = [];
+    $scope.guessLimit = 6;
+    $scope.displayWord = Array(word.length).join('_ ');
+    console.log($scope.displayWord);
+
+    
+  }
+  newGame();
+
+}]);

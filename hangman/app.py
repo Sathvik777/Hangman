ca26eeee57f23d748fd14a0d464082e1111486d3
Sathvik777 <katam.sathvik@gmail.com>
2017-12-17 22:00:56 +0100

    generic error handling

--- hangman/app.py
+++ hangman/app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, request, jsonify
-#from flask.ext.socketio import SocketIO, emit
+from flask.ext.api import status
 from models.UserSessions import UserSessions
 import json
 import gameLogic
@@ -20,8 +20,12 @@ def mainIndex():
 @app.route('/login', methods=['GET'])
 def login():
     username = request.args.get('username')
-    response = UserSessionsCache.build_authentication_response(username)
-    return jsonify(response)
+    if username in None :
+        return status.HTTP_401_UNAUTHORIZED
+    else:
+        response = UserSessionsCache.build_authentication_response(username)
+        return jsonify(response)
+    
 
 
 @app.route('/start', methods=['POST'])
@@ -29,8 +33,12 @@ def game_start():
 
     request.get_data()
     json_resquest = request.json
-    response = gameLogic.start_game(json_resquest.get('session_key'))
-    return jsonify(response)
+    session_key = json_resquest.get('session_key')
+    if session_key in None :
+        return status.HTTP_401_UNAUTHORIZED
+    else:    
+        response = gameLogic.start_game()
+        return jsonify(response)
 
 
 
@@ -39,13 +47,17 @@ def game_end():
     request.get_data()
     json_resquest = request.json
     session_key = json_resquest.get('session_key')
-    score = json_resquest.get('score')
-    username = UserSessionsCache.get_username_with_sessionKey(session_key)
-
-    gameLogic.end_game(session_key, score, username)
-
-    return jsonify({"status": "ok"})
-
+    if session_key in None :
+        return status.HTTP_401_UNAUTHORIZED
+    else:
+        score = json_resquest.get('score')
+        username = UserSessionsCache.get_username_with_sessionKey(session_key)
+        try:
+            gameLogic.end_game(session_key, score, username)
+            return jsonify({"status": "ok"})
+        except KeyError:
+            return status.HTTP_400_BAD_REQUEST
+        
 
 @app.route('/leaderboard', methods=['GET'])
 def get_leaderboard():
